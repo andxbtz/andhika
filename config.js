1230815cd6a1b343a05b83efe6a0817f6c6042f4
andxbtz <108200672+andxbtz@users.noreply.github.com>
2022-06-29 11:56:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -76,7 +76,7 @@ global.APIKeys = { // APIKey Here
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://pencarikode.xyz': 'pais', 
   'https://leyscoders-api.herokuapp.com': 'dappakntlll',
-  'https://zekais-api.herokuapp.com': 'apikeymu',
+  'https://zekais-api.herokuapp.com': '9YcHCcNM',
   'https://api.lolhuman.xyz': 'Deffbotz',
 }
 
